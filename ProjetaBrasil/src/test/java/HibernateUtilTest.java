49a839d8c2e69f988d5d379882a85501f2218a30
lucasdemm <lucasdemm@192.168.0.18>
2017-03-28 22:22:27 -0300

    teste

---
+++ ProjetaBrasil/src/test/java/HibernateUtilTest.java
@@ -0,0 +1,135 @@
+import java.text.ParseException;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Property;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.criterion.Subqueries;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import br.com.projetabrasil.model.dao.PessoaDAO;
+import br.com.projetabrasil.model.dao.Pessoa_Enum_Aux_Perfil_PessoasDAO;
+import br.com.projetabrasil.model.dao.UsuarioDAO;
+import br.com.projetabrasil.model.entities.Enum_Aux_Perfil_Pessoa;
+import br.com.projetabrasil.model.entities.Enum_Aux_Sexo;
+import br.com.projetabrasil.model.entities.Enum_Aux_Sim_ou_Nao;
+import br.com.projetabrasil.model.entities.Enum_Aux_Tipo_Identificador;
+import br.com.projetabrasil.model.entities.Pessoa;
+import br.com.projetabrasil.model.entities.Pessoa_Enum_Aux_Perfil_Pessoa;
+import br.com.projetabrasil.model.entities.Pessoa_Vinculo;
+import br.com.projetabrasil.model.entities.Usuario;
+import br.com.projetabrasil.util.HibernateUtil;
+import br.com.projetabrasil.util.Utilidades;
+
+public class HibernateUtilTest {
+
+	private Pessoa p;
+	private Usuario us;
+	private Pessoa_Enum_Aux_Perfil_Pessoa pp;
+
+	@SuppressWarnings("unused")
+	@Ignore
+	@Test
+	public void calendario() {
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+		//Criteria crit = null;
+		//DetachedCriteria subQuery1 = null;
+
+		DetachedCriteria subQuery1 = DetachedCriteria.forClass(Pessoa_Vinculo.class).setProjection(Property.forName("id_pessoa_d"));
+		subQuery1.add(Restrictions.eq("id_pessoa_m", 1l));
+
+		Criteria crit = sessao.createCriteria(Pessoa.class).setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY)
+				.add(Restrictions.and(Subqueries.propertyIn("id", subQuery1)));
+
+	}
+
+	@Test
+	public void cadastraPessoa() throws ParseException {
+		p = new Pessoa();
+		p.setCpf_Cnpj("74473450678");
+		p.setCpf_Cnpj(Utilidades.retiraCaracteres(p.getCpf_Cnpj()));
+
+		boolean vf = false;
+		if (p.getCpf_Cnpj().length() == 11)
+			vf = Utilidades.isValidCPF(p.getCpf_Cnpj());
+		else if (p.getCpf_Cnpj().length() == 14)
+			vf = Utilidades.isValidCNPJ(p.getCpf_Cnpj());
+
+		if (vf) {
+
+			p.setIdentificador(p.getCpf_Cnpj());
+			PessoaDAO pDAO = new PessoaDAO();
+			p = pDAO.retornaPelaIdentificacao(p.getIdentificador());
+			
+			System.out.println("Aqui é uma pessoa: "+p);
+
+			if (p == null) {
+				p = new Pessoa();
+				p.setCpf_Cnpj("08096819941");
+				p.setIdentificador(p.getCpf_Cnpj());
+				p.setRg_Insc("3739787");
+				p.setSexo(Enum_Aux_Sexo.MASCULINO);
+				p.setFone_1("48996453129");
+				p.setFone_2("");
+				p.setFone_3("");
+				p.setEmail("lucas.dmm@hotmail.com");
+				p.setId_Pessoa_Registro(p);
+				p.setAutoPontuacao(Enum_Aux_Sim_ou_Nao.NAO);
+				p.setEnum_Aux_Tipo_Identificador(Enum_Aux_Tipo_Identificador.CPF);
+				p.setDescricao("Lucas De Martino Macedo");
+				p.setFantasia_Apelido("");
+				p.setDataNascimento(Utilidades.retornaData("23/09/1978"));
+			}
+			p = pDAO.merge(p);
+			us = new Usuario();
+			us.setPessoa(p);
+
+			UsuarioDAO usDAO = new UsuarioDAO();
+			us = usDAO.retornaUsuarioPelaPessoa(p);
+
+			if (us == null) {
+				try {
+					us = new Usuario();
+					us.setPessoa(p);
+					us.setAtivo(true);
+					us.setSenhaSemCript("11111111");
+					us.setConfSenha("11111111");
+					//us.setId(null);
+					us.setId_Empresa(1);
+					us.setId_Pessoa_Registro(p);
+					us.setPessoa(p);
+					
+				} catch (RuntimeException error) {
+					error.printStackTrace();
+					throw error;
+				}
+			}
+			
+			us = usDAO.merge(us);
+
+			pp = new Pessoa_Enum_Aux_Perfil_Pessoa();
+			Pessoa_Enum_Aux_Perfil_PessoasDAO ppDAO = new Pessoa_Enum_Aux_Perfil_PessoasDAO();
+
+			pp.setId_pessoa(p);
+			pp.setEnum_Aux_Perfil_Pessoa(Enum_Aux_Perfil_Pessoa.ADMINISTRADORES);
+
+			pp = ppDAO.retornaPerfildaPessoaPelaPessoa(pp);
+
+			if (pp == null) {
+				pp = new Pessoa_Enum_Aux_Perfil_Pessoa();
+				pp.setId_pessoa(p);
+				pp.setEnum_Aux_Perfil_Pessoa(Enum_Aux_Perfil_Pessoa.ADMINISTRADORES);
+				pp.setId_Empresa(1);
+
+				pp.setId_Pessoa_Registro(p);
+				pp.setUltimaAtualizacao(Utilidades.retornaCalendario());
+			}
+			pp = ppDAO.merge(pp);
+		}
+	}
+		
+	
+		
+}	
